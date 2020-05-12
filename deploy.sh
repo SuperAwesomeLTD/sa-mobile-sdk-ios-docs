694ae03a0780c27f059dfae8f647ee786dda0735
Nicolas Tresegnie <nicolas.tresegnie@gmail.com>
2020-05-12 16:51:10 +0100

    Add Cloudfront invalidation to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+#!/bin/bash -e
 BRANCH="$1"
 
-if [ $1 = "develop" ]; then
+if [[ $1 = "develop" ]]; then
     aws --region=us-west-1 s3 sync "build/html/" s3://doc.staging.superawesome.tv/sa-mobile-sdk-ios/
 else
     aws --region=us-west-1 s3 sync "build/html/" s3://doc.superawesome.tv/sa-mobile-sdk-ios/
+    aws cloudfront create-invalidation --distribution-id E3HBU8LMG9T4OL --paths "*"
 fi
