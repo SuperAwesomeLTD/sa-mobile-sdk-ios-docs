3f5ba252daa3678fbb5bacd68c6ef54edb64214b
Gabriel Coman <dev.gabriel.coman@gmail.com>
2020-05-27 14:39:35 +0100

    fix(invalidation): removed cache invalidation:

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ if [[ $1 = "develop" ]]; then
     aws --region=us-west-1 s3 sync "build/html/" s3://doc.staging.superawesome.tv/sa-mobile-sdk-ios/
 else
     aws --region=us-west-1 s3 sync "build/html/" s3://doc.superawesome.tv/sa-mobile-sdk-ios/
-    aws cloudfront create-invalidation --distribution-id E3HBU8LMG9T4OL --paths "*"
+    # aws cloudfront create-invalidation --distribution-id E3HBU8LMG9T4OL --paths "*"
 fi
